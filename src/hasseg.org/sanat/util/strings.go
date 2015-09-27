f3c0ce2053fa4ebf51b0a8b670adb8bf5871292d
Ali Rantakari <visit-with-web-browser@hasseg.org>
2015-09-28 00:45:24 +0300

    Fix comma-separated list parsing
    Return {} instead of {“”} for empty string

--- src/hasseg.org/sanat/util/strings.go
+++ src/hasseg.org/sanat/util/strings.go
@@ -6,6 +6,9 @@ import (
 
 func ComponentsFromCommaSeparatedList(text string) []string {
 	ret := make([]string, 0)
+	if len(strings.TrimSpace(text)) == 0 {
+		return ret
+	}
 	for _, s := range strings.Split(text, ",") {
 		ret = append(ret, strings.TrimSpace(s))
 	}

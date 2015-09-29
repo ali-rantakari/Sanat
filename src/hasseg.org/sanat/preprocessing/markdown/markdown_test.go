3ed70a9d4b5b69c6896e0e4862b872c3746cd8c0
Ali Rantakari <visit-with-web-browser@hasseg.org>
2015-09-29 17:53:31 +0300

    Add tests for the Markdown preprocessor

---
+++ src/hasseg.org/sanat/preprocessing/markdown/markdown_test.go
@@ -0,0 +1,31 @@
+package markdown_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"hasseg.org/sanat/preprocessing/markdown"
+)
+
+func TestMarkdownToHTML(t *testing.T) {
+	pp := markdown.PreProcessor{}
+
+	ass := func(expected string, input string) {
+		assert.Equal(t, expected, pp.ProcessRawValue(input), input)
+	}
+
+	ass("", "")
+	ass("Hello", "Hello")
+
+	// Basic Markdown on individual word
+	ass("<em>Hello</em>", "_Hello_")
+	ass("<em>Hello</em>", "*Hello*")
+	ass("<strong>Hello</strong>", "__Hello__")
+	ass("<strong>Hello</strong>", "**Hello**")
+	ass("<code>Hello</code>", "`Hello`")
+
+	// Preserves leading/trailing whitespace
+	ass("\t<em>Hello</em>\t", "\t_Hello_\t")
+	ass("  <em>Hello</em>  ", "  _Hello_  ")
+}
